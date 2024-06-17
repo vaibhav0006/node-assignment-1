2851be35e57e2f56bf757d1f835c484c83946056
vaibhav0006 <vaibhavrajput0006@gmail.com>
2024-06-17 23:33:47 +0530

    initial commit

---
+++ http.js
@@ -0,0 +1,35 @@
+const http = require("http");
+const fs = require("fs");
+const url = require("url");
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/favicon.ico") return res.end();
+  const log = `${Date.now()} : ${req.url} New request received\n`;
+  const myURL = url.parse(req.url, true);
+
+  fs.appendFile("log.txt", log, (err, data) => {
+    if (err) {
+      console.error("Error appending to log file:", err);
+    }
+    switch (myURL.pathname) {
+      case "/":
+        res.end("HomePage");
+        break;
+      case "/about":
+        const username = myURL.query.name;
+        res.end(`Hi ${username}`);
+        break;
+      case "/contact":
+        if (req.method === "GET") {
+          res.end("ContactPage");
+        }
+        break;
+      default:
+        res.end("404 not found");
+    }
+  });
+});
+
+server.listen(5000, () => {
+  console.log("Server Running on PORT 5000");
+});
